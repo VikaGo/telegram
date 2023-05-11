52eb4e994f3c08a9e99261f8e248ec93126cb265
VikaGo <88908209+VikaGo@users.noreply.github.com>
2023-05-11 15:33:53 +0300

    Update config.go

--- configs/config.go
+++ configs/config.go
@@ -5,4 +5,6 @@ type Config struct {
 	LogLevel      string `env:"LOG_LEVEL"`
 	logFilePath   string `env:"LOG_FILE_PATH"`
 	logFormat     string `env:"LOG_FORMAT"`
+	apiKey        string `env:"apiKey"`
 }
+
